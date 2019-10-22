ae57f5eecc5f8c2534cafd3033e54dfa151243b0
robertobirijnr <robertobirijnr@hotmail.com>
2019-10-22 15:08:32 +0100

    login route

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync =require('../utils/catchAsync')
 
-exports.getOverview =catchAsync(async(req,res)=>{
+exports.getOverview =catchAsync(async(req,res,next)=>{
     const tours = await Tour.find();
     res.status(200).render('overview',{
       title:'All Tours',
@@ -9,13 +9,20 @@ exports.getOverview =catchAsync(async(req,res)=>{
     })
   })
 
-exports.getTour = catchAsync(async(req,res)=>{
+exports.getTour = catchAsync(async(req,res,next)=>{
     const tour = await Tour.findOne({slug:req.params.slug}).populate({
         path:'reviews',
         fields:'review rating user'
     })
     res.status(200).render('tour.pug',{
-      title:'Kakuum Forest',
+      title:`${tour.name}`,
       tour
     })
   });
+
+
+  exports.getLogin =(req,res)=>{
+    res.status(200).render('login',{
+        title:'Log into account'
+    })
+  }
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
   
   
-  router.get('/',viewsController.getOverview)
-  router.get('/tour/:slug',viewsController.getTour)
+  router.get('/',viewsController.getOverview);
+  router.get('/tour/:slug',viewsController.getTour);
+  router.get('/login',viewsController.getLogin)
 
 module.exports = router;
