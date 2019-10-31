72c183c439b9884f4bf15d160b3bce5d297d8dbf
robertobirijnr <robertobirijnr@hotmail.com>
2019-10-31 15:29:23 +0000

    creating email template

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/AppError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 const crypto = require('crypto');
 
 const signToken = id =>{
@@ -43,7 +43,11 @@ exports.user_signup= catchAsync(async(req,res,next)=>{
         role:req.body.role,
         password:req.body.password,
         passwordConfirm:req.body.passwordConfirm
-    })
+    });
+    // const url = 'http://localhost:3000/me';
+    const url = `${req.protocol}://${req.get('host')}/me`;
+    console.log(url);
+    await new Email(newUser,url).sendWelcome();
     createSendToken(newUser,201,res);
    
 })
@@ -178,11 +182,11 @@ exports.restrictTo = (...roles) => {
     const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
   
     try {
-      await sendEmail({
-        email: user.email,
-        subject: 'Your password reset token (valid for 10 min)',
-        message
-      });
+      // await sendEmail({
+      //   email: user.email,
+      //   subject: 'Your password reset token (valid for 10 min)',
+      //   message
+      // });
   
       res.status(200).json({
         status: 'success',
